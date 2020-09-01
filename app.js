35926793b93463d43ab375f13cecc645eccb544e
Ben Dunn <benthedunn@gmail.com>
2020-09-01 14:09:10 -0600

    characters on page

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
-$suki = document.querySelector('.suki')
-$fan = document.querySelector('.invisible')
-$cardContainer =  document.querySelector('cardContainer')
+const baseURL = 'http://localhost:3000/characters'
 
-$suki.addEventListener('click', bending)
+const $cardContainer = document.querySelector('.cardContainer')
+const $containerContainer = document.querySelector('.containerContainer')
+
+const gaang = [
+    '5cf5679a915ecad153ab68c9',
+    '5cf5679a915ecad153ab6a45',
+    '5cf5679a915ecad153ab6993',
+    '5cf5679a915ecad153ab6a54',
+    '5cf5679a915ecad153ab6ab8',
+    '5cf5679a915ecad153ab6a70',
+]
+
+fetch(baseURL)
+    .then(response => response.json())
+    .then(objects => getCharacters(objects))
+
+function getCharacters(objects){
+    objects.forEach(object => showCharacter(object))
+}
+
+function showCharacter(object){
+    const $img = document.createElement('img')
+    const $li = document.createElement('li')
+    $li.className = 'cardContainer'
+    $img.src = object.url
+    $img.alt = object.name
+    if(gaang.includes(object.api_id)){
+        $li.append($img)
+        $containerContainer.appendChild($li)
+        handleClick($li)
+    }
+}
+
+function handleClick(card){
+    card.addEventListener('click', bending)
+}
+
+
 
 function bending (){
-    $fan.className = 'show'
-    setTimeout(() => {$fan.className = 'invisible'}, 2000) 
+    if (event.target.alt == 'Aang'){
+        console.log('its aang')
+    }
 }
+
+
+
+// function bending (){
+//     $fan.className = 'show'
+//     setTimeout(() => {$fan.className = 'invisible'}, 2000) 
+// }
